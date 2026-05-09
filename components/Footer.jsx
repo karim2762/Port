917a0bb65f1d52cd5bf2daddbe064e30cfbea684
Kareem Basha <karimullaskmk1234@gmail.com>
2026-05-09 10:37:56 +0530

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+  return (
+    <footer className="py-8 text-center text-gray-500 border-t border-white/10">
+      <p>Built with passion by Kareem Basha.</p>
+    </footer>
+  )
+}
+
+

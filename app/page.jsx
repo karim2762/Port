0283a6a2df66cde9b9c40c9f6f46269641993b5e
Kareem Basha <karimullaskmk1234@gmail.com>
2026-05-09 10:35:41 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,14 @@
+
 'use client'
 
-import Navbar from '@/components/Navbar'
-import Hero from '@/components/Hero'
-import About from '@/components/About'
-import Skills from '@/components/Skills'
-import Projects from '@/components/Projects'
-import Contact from '@/components/Contact'
-import Footer from '@/components/Footer'
-import BackgroundAnimation from '@/components/BackgroundAnimation'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+import BackgroundAnimation from '../components/BackgroundAnimation'
 
 export default function Home() {
   return (
